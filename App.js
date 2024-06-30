7ab4fa82b9e681778dd833e38e3cbab29617f37e
ThanhB <bintri92@gmail.com>
2024-06-30 14:34:09 +0700

    update

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput, Alert } from "react-native";
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-red-500">Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
     </View>
   );
 }
 
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+    </View>
+  );
+}
+const Stack = createStackNavigator();
 
+export default function App() {
+  return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    paddingTop: 100,
+  },
+});
