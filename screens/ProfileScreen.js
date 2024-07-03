02d08a3c534e83ddaf112160de193fc1626404cc
Vo Anh Hieu <voanhhieu10250@gmail.com>
2024-07-03 18:51:57 +0700

    add logout btn to profile screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useContext } from "react";
+import { Button, Text } from "react-native";
+import { Context as AuthContext } from "../context/AuthContext";
 
 const ProfileScreen = () => {
+  const { signout } = useContext(AuthContext);
   return (
-    <Text>ProfileScreen</Text>
-  )
-}
+    <>
+      <Text>ProfileScreen</Text>
+      <Button title="Sign Out" onPress={signout} />
+    </>
+  );
+};
 
-export default ProfileScreen
+export default ProfileScreen;
