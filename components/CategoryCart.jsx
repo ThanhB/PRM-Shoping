e9eaaa9e2032c1a604e49257381243a27e7c61b0
Vo Anh Hieu <voanhhieu10250@gmail.com>
2024-07-07 00:00:52 +0700

    9999px causes error

--- components/CategoryCart.jsx
+++ components/CategoryCart.jsx
@@ -11,7 +11,6 @@ const CategoryCart = () => {
               width: "100%",
               height: "100%",
               objectFit: "cover",
-              borderRadius: "9999px",
             }}
           />
         </View>
