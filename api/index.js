39ba3afa31604fbb3c4736711840ffe473b896d1
Vo Anh Hieu <voanhhieu10250@gmail.com>
2024-07-15 05:18:16 +0700

    clear the token and redirect user back to login screen whenevery api return 401 error (token expired)

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
+import { navigateToAuth } from "../navigationRef";
 
 const apiInstance = axios.create({
   baseURL: "https://api-shop-ic3t.onrender.com/api",
@@ -14,6 +15,19 @@ apiInstance.interceptors.request.use(
     return config;
   },
   (err) => {
+    return Promise.reject(err);
+  }
+);
+
+apiInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (err) => {
+    if (err.response.status === 401) {
+      await AsyncStorage.removeItem("token");
+      navigateToAuth();
+    }
     return Promise.reject(err);
   }
 );
